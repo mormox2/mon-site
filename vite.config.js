738a02caa3c0c473f6d07646c254f795b590ac39
Dr Mossaab <133013388+mormox2@users.noreply.github.com>
2026-05-01 00:33:28 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/mon-site/'
+  base: '/mon-site/',
+  
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html', // point d'entrée explicite
+    },
+  },
+
+  server: {
+    open: true, // ouvre le navigateur automatiquement en dev
+  },
 })
